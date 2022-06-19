c2a4be4e48acd22b0f3b64333a308a52c2370607
Ruslan Pegov <ruslan.pegov@gmail.com>
2022-06-19 22:36:57 +0300

    New project structure
    The structure of the project needs to be changed in accordance with the rules of Aurora IDE.
    Also added Russian translations.
    
    Relates #1

---
+++ org.ilyavysotsky.yasailmusic.pro
@@ -0,0 +1,33 @@
+TARGET = org.ilyavysotsky.yasailmusic
+
+CONFIG += \
+    sailfishapp
+
+PKGCONFIG += \
+
+SOURCES += \
+    src/apirequest.cpp \
+    src/authorization.cpp \
+    src/cacher.cpp \
+    src/downloader.cpp \
+    src/models/playlistmodel.cpp \
+    src/main.cpp \
+
+HEADERS += \
+    src/apirequest.h \
+    src/authorization.h \
+    src/cacher.h \
+    src/downloader.h \
+    src/models/playlistmodel.h
+
+DISTFILES += \
+    rpm/org.ilyavysotsky.yasailmusic.yaml \
+    LICENSE \
+    README.md \
+
+SAILFISHAPP_ICONS = 86x86 108x108 128x128 172x172
+
+CONFIG += sailfishapp_i18n
+
+TRANSLATIONS += \
+    translations/org.ilyavysotsky.yasailmusic-ru.ts \
--- translations/org.ilyavysotsky.yasailmusic-ru.ts
+++ translations/org.ilyavysotsky.yasailmusic-ru.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
 <context>
     <name>LoginPage</name>
     <message>
         <location filename="../qml/pages/LoginPage.qml" line="26"/>
         <source>Login:</source>
-        <translation type="unfinished"></translation>
+        <translation>Логин:</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoginPage.qml" line="40"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Пароль:</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoginPage.qml" line="56"/>
         <source>Code:</source>
-        <translation type="unfinished"></translation>
+        <translation>Код:</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoginPage.qml" line="76"/>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Войти</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoginPage.qml" line="91"/>
         <source>Logged into Yandex Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Успешный вход в Яндекс Музыку</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoginPage.qml" line="95"/>
         <source>Login fail!</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка входа!</translation>
     </message>
 </context>
 <context>
@@ -39,37 +39,37 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="22"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Поиск</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="26"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Выход</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="34"/>
         <source>View playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Просмотр плейлиста</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="41"/>
         <source>My wave</source>
-        <translation type="unfinished"></translation>
+        <translation>Моя волна</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="47"/>
         <source>Search audio...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/MainPage.qml" line="101"/>
-        <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Поиск аудио...</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="101"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>Пауза</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="101"/>
+        <source>Play</source>
+        <translation>Играть</translation>
     </message>
 </context>
 <context>
@@ -77,7 +77,7 @@
     <message>
         <location filename="../qml/pages/PlaylistPage.qml" line="15"/>
         <source>Current tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Текущие треки</translation>
     </message>
 </context>
 <context>
@@ -85,12 +85,12 @@
     <message>
         <location filename="../qml/YaSailMusic.qml" line="55"/>
         <source>Loading</source>
-        <translation type="unfinished"></translation>
+        <translation>Загрузка</translation>
     </message>
     <message>
         <location filename="../qml/YaSailMusic.qml" line="56"/>
         <source>tags...</source>
-        <translation type="unfinished"></translation>
+        <translation>тэги...</translation>
     </message>
 </context>
 </TS>
